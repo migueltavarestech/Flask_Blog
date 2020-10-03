f168cd190531a761db3db97ecd193d99cdd977d1
Miguel <mike.lo.123@hotmail.com>
2020-10-03 10:49:36 +0100

    Add comments throughout the code

--- flaskblog.py
+++ flaskblog.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd8bc59a7ef0c8ae183ef4fd0bec66836'
 
+# Dummy Post Data
 posts = [
     {
         'author': 'Miguel Tavares',
@@ -19,16 +20,20 @@ posts = [
     }
 ]
 
+# Home Page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
 
+# About Page
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+
+# Register Page w/ GET and POST possibilities
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -37,6 +42,8 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+
+# Login Page w/ GET and POST possibilities
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -49,5 +56,6 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+# Use python script to open in localhost instead of opening through flask
 if __name__ == '__main__':
     app.run(debug=True)
