507230837dfeaa5d5b637d625329cb254ab72d8c
Miguel <mike.lo.123@hotmail.com>
2020-10-03 17:11:12 +0100

    Create dummy database

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,41 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd8bc59a7ef0c8ae183ef4fd0bec66836'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# Create SQLAlchemy User models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    #One user can have multiple posts
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    # How our object is printed out
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# Create SQLAlchemy Posts models
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    #One post can only have one user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable='False')
+
+    # How our object is printed out
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 # Dummy Post Data
 posts = [
