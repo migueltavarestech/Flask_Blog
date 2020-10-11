737cb695fcb886337b1e1e9c301908a8bf18ec29
Miguel <mike.lo.123@hotmail.com>
2020-10-11 12:03:21 +0100

    Reset Password (Functions + Email)

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
-from flaskblog import db, login_manager
 from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 # Return user details
@@ -17,6 +18,21 @@ class User(db.Model, UserMixin):
 
     # One user can have multiple posts
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    # Expire Token for Reset Password Link
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    # Verify token
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     # How our object is printed out
     def __repr__(self):
