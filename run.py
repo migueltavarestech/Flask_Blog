e797124457187528e765b4533818e13a265603fb
Miguel <mike.lo.123@hotmail.com>
2020-10-11 18:35:23 +0100

    Create function create_app()

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblog import app
+from flaskblog import create_app
+
+app = create_app()
 
 # Use python script to open in localhost instead of opening through flask
 if __name__ == '__main__':
